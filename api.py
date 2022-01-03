9fd962f1d29ed1535d4c86dcf2633b48333d1493
90sbudditha@gmail.com <admin@example.com>
2022-01-03 17:48:32 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,23 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+class Crypto:
+    def get_top_10(self):
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+        parameters = {
+        'start':'1',
+        'limit':'1',
+        'convert':'USD'
+        }
+        headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '6357af22-7052-4826-83de-12a44c7f12fb',
+        }
+
+        session = Session()
+        session.headers.update(headers)
+
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        return data['data']
